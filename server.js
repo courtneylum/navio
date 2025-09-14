54249fdd568d385cf6be0305a93aba22d3f64354
Courtney Lum <courtney.jlum@gmail.com>
2025-09-13 23:43:22 -0700

    Update file name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const TravelModel = require("./model/travel");
+const TravelModel = require("./model/Travel");
 const callAzureApi = require("./azureOpenAi");
 
 const app = express();
